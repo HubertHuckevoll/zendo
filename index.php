14ecf740007fd0d5b73dc9ef3cac7c944c6adb8c
HubertHuckevoll <konstantin.meyer@gmail.com>
2023-06-25 10:55:08 +0200

    last check in before switch to coins lib

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
 
-require_once('./lib/loader.php');
+require_once('./loader.php');
+require_once('./lib/logger.php');
 require_once('../RecipeJS/RecipeJS.php');
 
 $a = new pRouter('mainC', 'index');
-$a->run()
+$a->runWithPathInfo();
 
 ?>
--- lib/pRouter.php
+++ lib/pRouter.php
@@ -11,41 +11,80 @@ class pRouter
     $this->mainMethodName = $mainMethodName;
   }
 
-  public function run()
+  /**
+   * run with a path pattern of index.php/mod/hook?queryparams=xyz
+   * ________________________________________________________________
+   */
+  public function runWithPathInfo()
   {
     $dummy = '';
-    $obj = null;
     $pathInfo = '';
-    $objName = '';
+    $modName = '';
     $methodName = '';
 
     if (isset($_SERVER['PATH_INFO']))
     {
       $pathInfo = $_SERVER['PATH_INFO'];
-      list($dummy, $objName, $methodName) = explode('/', $pathInfo); // $dummy => PATH_INFO has a leading "/" that creates a fake first entry
+      list($dummy, $modName, $methodName) = explode('/', $pathInfo); // $dummy => PATH_INFO has a leading "/" that creates a fake first entry
     }
     else
     {
-      $objName = $this->mainControllerName;
+      $modName = $this->mainControllerName;
       $methodName = $this->mainMethodName;
     }
 
+    $this->exec($modName, $methodName);
+  }
+
+  /**
+   * run with two get vars: index.php?mod=controller&hook=methodName
+   * ________________________________________________________________
+   */
+  public function runWithGetVarsModHook()
+  {
+    $modName = '';
+    $methodName = '';
+
+    $modName = filter_input(INPUT_GET, 'mod');
+    $methodName = filter_input(INPUT_GET, 'hook');
+
+    if (!$modName)
+    {
+      $modName = $this->mainControllerName;
+    }
+
+    if (!$methodName)
+    {
+      $methodName = $this->mainMethodName;
+    }
+
+    $this->exec($modName, $methodName);
+  }
+
+  /**
+   * call the controller
+   * ________________________________________________________________
+   */
+  protected function exec(string $modName, string $methodName): void
+  {
+    $controllerObj = null;
+
     try
     {
-      $obj = new $objName();
+      $controllerObj = new $modName();
 
-      if ((isset($obj) && method_exists($obj, $methodName)))
+      if ((isset($controllerObj) && method_exists($controllerObj, $methodName)))
       {
-        call_user_func(array($obj, $methodName));
+        call_user_func(array($controllerObj, $methodName));
       }
       else
       {
-        die('Fatal error: Couldn\'t run method "'.$methodName.'" on object "'.$objName.'".');
+        die('Fatal error: Couldn\'t run method "'.$methodName.'" on object "'.$controllerObj.'".');
       }
     }
     catch(Exception $e)
     {
-      die('Fatal error: Couldn\'t instantiate object "'.$objName.'".');
+      die('Fatal error: Couldn\'t instantiate object "'.$controllerObj.'".');
     }
   }
 }
